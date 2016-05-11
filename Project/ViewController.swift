d774ad6909b696875bac62966df927cebdcd0378
Artyom Mazurkevich <artyom.mazurkevich@gmail.com>
2016-05-11 03:54:40 +0300

    encrypt decrypt

--- Project/ViewController.swift
+++ Project/ViewController.swift
@@ -11,8 +11,34 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let documentsDirectory: AnyObject = paths[0].stringByAppendingString("/secrert.png")
+        
+        let dt = "senya"
+        let img = UIImage(named: "ORIGINAL_IMAGE")
+        ISSteganographer.hideData(dt, withImage: img) { (img, error) in
+            if ((error) != nil){
+                print("error")
+            }
+            else{
+                let temp = img as! UIImage
+                UIImagePNGRepresentation(temp)?.writeToFile(documentsDirectory as! String, atomically: true)
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let img1 = UIImage(named: "secrert")
+        ISSteganographer.dataFromImage(img1) { (data, error) in
+            if ((error) != nil){
+                print("error")
+            }
+            else{
+                let hiddenData = NSString(data: data, encoding: 8)
+                print(hiddenData)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
