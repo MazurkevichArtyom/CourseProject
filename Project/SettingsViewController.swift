e95d9d8ff72c64e230117daeb4e9f3235b80503b
Artyom Mazurkevich <artyom.mazurkevich@gmail.com>
2016-05-18 15:02:40 +0300

    work with UI

---
+++ Project/SettingsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SettingsViewController.swift
+//  Project
+//
+//  Created by Artyom Mazurkevich on 17/05/16.
+//  Copyright © 2016 Artyom Mazurkevich. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "pixels")!)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
